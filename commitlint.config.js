8b1234171ea6707dd868f3d3ce4313e4d54b0e13
vv0rkz <ivanchebykin4@gmail.com>
2026-04-07 13:44:39 +0500

    chore: jst, pnpm

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,13 @@
+let types = ['feat', 'fix', 'refactor', 'build', 'chore', 'docs', 'perf']
+
+try {
+  const { default: config } = await import('./jst.config.js')
+  if (config.commits?.types) types = config.commits.types
+} catch {}
+
 export default {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [2, "always", ["feat", "fix", "refactor", "build", "chore", "docs"]],
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', types],
+  },
 }
